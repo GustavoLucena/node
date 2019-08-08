061cd20d8529fd4b3840264cac3c507bd50cb84e
DragonYorozuya <dragon6_@hotmail.com>
2019-08-08 12:00:22 -0300

    3

--- index.js
+++ index.js
@@ -10,6 +10,4 @@ app.get('/', function (req, res) {
 const PORT = process.env.PORT || 3000;
 
 
-app.listen(PORT, function () {
-  console.log('Example app listening on port 3000!');
-});
+app.listen(PORT);
